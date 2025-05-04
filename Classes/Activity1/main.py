f4f594ea2403bfc18055c82eb976c496965f2c0a
CornyShawny <160194015+CornyShawny@users.noreply.github.com>
2025-05-05 09:33:56 +1000

    Update main.py

--- Classes/Activity1/main.py
+++ Classes/Activity1/main.py
@@ -11,10 +11,10 @@ player_list = [Player('Gregor', 'Knight', 'Protector', 20, 240),
 # TODO: Create an instance of Weapon with random damage between 12 and 15
 weapon_sword = Weapon('Sword', 'Melee', random.randint(12, 15))
 
-weapon_list = [Weapon(), 
-               Weapon(), 
-               Weapon(), 
-               Weapon()]
+weapon_list = [Weapon('LongSword', 'Melee', random.randint(20, 25)), 
+               Weapon('Staff', 'Ranged', random.randint(25, 30)),
+               Weapon('Bow', 'Ranged', random.randint(15, 20)),
+               Weapon('LongBow', 'Ranged', random.randint(20, 25))]
 # TODO: Create an instance of Enemy with random damage between 15 and 18, and random health between 80 and 140
 enemy_chief = Enemy('Chief', 'Goblin', random.randint(15, 18), random.randint(80, 140))
 
@@ -35,3 +35,33 @@ print(f"Enemy Name: {enemy_chief.name}")
 print(f"Enemy Race: {enemy_chief.race}")
 print(f"Enemy Damage: {enemy_chief.dmg}")
 print(f"Enemy Health: {enemy_chief.health}")
+
+def select():
+    cselection = int(input('Choose a Character (1-4): '))
+    if cselection == 1:
+        current = player_list[0].name
+        print(current)
+    elif cselection == 2:
+        current = player_list[1].name
+        print(current)
+    elif cselection == 3:
+        current = player_list[2].name
+        print(current)
+    elif cselection == 4:
+        current = player_list[3].name
+        print(current)
+
+    wselection = int(input('Choose a Weapon (1-4): '))
+    if wselection == 1:
+        current = weapon_list[0].name + weapon_list[0].category
+        print(current)
+    elif wselection == 2:
+        current = weapon_list[1].name
+        print(current)
+    elif wselection == 3:
+        current = weapon_list[2].name
+        print(current)
+    elif wselection == 4:
+        current = weapon_list[3].name
+        print(current)
+select()
