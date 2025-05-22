c15a546df98f3a50cc798edae03e904dbe32e15d
CornyShawny <160194015+CornyShawny@users.noreply.github.com>
2025-05-22 15:09:27 +1000

    e

---
+++ Abstraction/Activity1/main.py
@@ -0,0 +1,4 @@
+from shapey import *
+
+circle1 = Circle(1)
+print(f"The volume is: {circle1.volume}")
--- Abstraction/Activity1/shapey.py
+++ Abstraction/Activity1/shapey.py
@@ -9,9 +9,9 @@ class Shape(ABC):
 	def perimeter(self):
 		pass
     
-    @abstractmethod
-    def volume(self):
-        pass
+	@abstractmethod
+	def volume(self):
+		pass
 
 class Circle(Shape):
 	def __init__(self, radius):
@@ -22,26 +22,33 @@ class Circle(Shape):
 	
 	def area(self):
 		return 3.14 * self.radius ** 2
+	
+	def volume(self):
+		return 3.14 * 4/3 * self.radius * self.radius * self.radius
 
 class Rectangle(Shape):
-	def __init__(self, width, height):
+	def __init__(self, width, height, length):
 		self.width = width
 		self.height = height
+		self.length = length
 		
-        def perimeter(self):
-	        return 2 * (self.width + self.height)
+	def perimeter(self):
+		return 2 * (self.width + self.height)
 	
 	def area(self):
 		return self.width * self.height
+	
+	def volume(self):
+		return self.width * self.height * self.length
 
 # Creating instances of concrete subclasses 
 circle = Circle(5) 
-rectangle = Rectangle(4, 6) 
+rectangle = Rectangle(4, 6, 10)
 
 # Using the same interface to compute perimeter 
 print("Circle Perimeter:", circle.perimeter()) 
 print("Rectangle Perimeter:", rectangle.perimeter()) 
 
 # Output
-Circle Perimeter: 31 
-Rectangle Perimeter: 20
+#Circle Perimeter: 31 
+#Rectangle Perimeter: 20
